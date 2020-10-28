35cc49f9a7e035cee94d1ebdc4dd0a651b3f5ee9
Colin Shanny <cshannyy@gmail.com>
2020-10-28 11:47:37 -0500

    inffered typing string interpolation ternary operator

---
+++ SyntaxAndSYntaxSugar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SyntaxAndSYntaxSugar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var answer = 4;
+            var response = (answer > 9) ? $"{answer} is less than nine" : $"{ answer} is greater or equal to nine";
+
+        Console.WriteLine(response);
+        }
+    }
+   
+            
+       
+}
